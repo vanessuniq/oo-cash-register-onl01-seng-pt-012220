01a33e9a8f52838c994b3352030f527ad3d6c34e
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-20 01:34:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   
   
   def add_item (title, price, quantity = 1)
-    @title = title
+    @@items_on_cart << title
     @total_price = price * quantity 
     @total += @total_price
   end
